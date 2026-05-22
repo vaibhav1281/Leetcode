ff7911bddea3a32c8a827237213daf7e18030fad
Vaibhav_Saroj <93854530+vaibhav1281@users.noreply.github.com>
2026-05-22 12:37:08 +0530

    Implement stack operations and add recursion example in C++

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,20 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    cout<<"Hello World";  
+
+stack<int>s;
+void stk() {
+    
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.emplace(5);
+}
+int main() {
+    stk();
+    while(!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    return 0;
 }
